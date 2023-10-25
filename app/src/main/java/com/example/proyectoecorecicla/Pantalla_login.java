8412956c7ed654698bd5da1e111b9806d4d53f00
S3rgio1995 <sergiolopezg.sl@gmail.com>
2023-10-24 22:13:48 -0500

    iniciosesion visual y funcional

--- app/src/main/java/com/example/proyectoecorecicla/Pantalla_login.java
+++ app/src/main/java/com/example/proyectoecorecicla/Pantalla_login.java
@@ -1,14 +1,29 @@
 package com.example.proyectoecorecicla;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 public class Pantalla_login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_login);
+
+        TextView registro = findViewById(R.id.labelregis);
+        CardView registarg = findViewById(R.id.Registroc);
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registarg.setVisibility(View.VISIBLE);
+            }
+        });
     }
+
 }
