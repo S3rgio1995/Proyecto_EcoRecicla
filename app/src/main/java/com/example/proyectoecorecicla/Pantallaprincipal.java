cd99b87ab17f550ba589b99ddc551064fc313cc9
S3rgio1995 <sergiolopezg.sl@gmail.com>
2023-10-29 22:28:13 -0500

    ultimos cambios primera entrega

--- app/src/main/java/com/example/proyectoecorecicla/Pantallaprincipal.java
+++ app/src/main/java/com/example/proyectoecorecicla/Pantallaprincipal.java
@@ -2,7 +2,10 @@ package com.example.proyectoecorecicla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Pantallaprincipal extends AppCompatActivity {
 
@@ -10,5 +13,38 @@ public class Pantallaprincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantallaprincipal);
+        Button pantcatego, pantrecomen, pantestad, panregiselem;
+        pantestad =findViewById(R.id.btnestadisp);
+        panregiselem = findViewById(R.id.Registelembt);
+        pantcatego = findViewById(R.id.pantcategobt);
+        pantcatego.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pasarpa1= new Intent(getApplicationContext(), pantallacategorias.class);
+                startActivity(pasarpa1);
+            }
+        });
+        pantrecomen = findViewById(R.id.pantrecomenbt);
+    pantrecomen.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent pasarpa2= new Intent(getApplicationContext(), Pantallaconsejos.class);
+            startActivity(pasarpa2);
+        }
+    });
+pantestad.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Intent pasarpa3= new Intent(getApplicationContext(), PantallaEstadisticas.class);
+        startActivity(pasarpa3);
+    }
+});
+panregiselem.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Intent pasarpa4= new Intent(getApplicationContext(), Pantallaregistro.class);
+        startActivity(pasarpa4);
+    }
+});
     }
 }
--- app/src/main/java/com/example/proyectoecorecicla/pantallacategorias.java
+++ app/src/main/java/com/example/proyectoecorecicla/pantallacategorias.java
@@ -17,11 +17,17 @@ public class pantallacategorias extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent pasarpa2= new Intent(getApplicationContext(),PantallaEstadisticas.class);
+        Intent pasarpa2= new Intent(getApplicationContext(),Pantallaregistro.class);
+        Intent pasarpa3= new Intent(getApplicationContext(), Pantallaprincipal.class);
         setContentView(R.layout.activity_pantallacategorias);
         Button boton1 = findViewById(R.id.Registrarresic);
         Button boton2 = findViewById(R.id.Btnatrascatego);
-
+boton2.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        startActivity(pasarpa3);
+    }
+});
 boton1.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View view) {
