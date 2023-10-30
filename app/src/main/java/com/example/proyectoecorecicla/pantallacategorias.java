e753088f6f72d7ed8a268659f5fad33e81ca8831
S3rgio1995 <sergiolopezg.sl@gmail.com>
2023-10-29 20:57:00 -0500

    cambios pantallas

--- app/src/main/java/com/example/proyectoecorecicla/pantallacategorias.java
+++ app/src/main/java/com/example/proyectoecorecicla/pantallacategorias.java
@@ -2,22 +2,31 @@ package com.example.proyectoecorecicla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
 import com.google.android.material.tabs.TabItem;
 
 public class pantallacategorias extends AppCompatActivity {
-
+    Intent pasarpa= new Intent(getApplicationContext(),PantallaEstadisticas.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantallacategorias);
+        Button boton1 = findViewById(R.id.Registrarresic);
+        Button boton2 = findViewById(R.id.Btnatrascatego);
 
-
+boton1.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        startActivity(pasarpa);
+    }
+});
 
     }
 
