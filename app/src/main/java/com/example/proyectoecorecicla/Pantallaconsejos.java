167132ca627381ad399108c48aaf6a9b85f5a018
S3rgio1995 <sergiolopezg.sl@gmail.com>
2023-11-20 14:14:54 -0500

    ultimos cambios primera entrega

---
+++ app/src/main/java/com/example/proyectoecorecicla/Pantallaconsejos.java
@@ -0,0 +1,15 @@
+package com.example.proyectoecorecicla;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Pantallaconsejos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantallaconsejos);
+
+    }
+}
---
+++ app/src/main/java/com/example/proyectoecorecicla/Pantallaregistro.java
@@ -0,0 +1,13 @@
+package com.example.proyectoecorecicla;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Pantallaregistro extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantallaregistro);
+    }
+}
