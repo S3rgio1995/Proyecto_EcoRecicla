c1c59cf4c94046c6f65993830a666a39bb229ed9
S3rgio1995 <sergiolopezg.sl@gmail.com>
2023-12-07 00:58:09 -0500

    correccion de errores y diseño estadisticas

--- app/src/main/java/com/example/proyectoecorecicla/PantallaEstadisticas.java
+++ app/src/main/java/com/example/proyectoecorecicla/PantallaEstadisticas.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -18,8 +20,10 @@ import java.util.ArrayList;
 
 public class PantallaEstadisticas extends AppCompatActivity {
 
-    TextView totalreciclajetextv, totextvpesorecic,textvviditot,papeleditext;
-    ProgressBar progBarVidrio,progBarPapel;
+    TextView totalreciclajetextv, totextvpesorecic,textvviditot,papeleditext,ttaoeditalumtex,ttaoeditplasticotext,materecitext;
+    ProgressBar progBarVidrio,progBarPapel,progBarAluminio,progPlastico;
+    Button masinfo;
+
 
     public ArrayList<Registroreciclaje> listregis (File Regis,String idus){
         ArrayList<Registroreciclaje> list= new ArrayList<>();
@@ -105,7 +109,62 @@ public class PantallaEstadisticas extends AppCompatActivity {
 
         return list;
     }
+    public ArrayList<Registroreciclaje> listconsulaluminio (File Regis,String idus,String items){
+        ArrayList<Registroreciclaje> list= new ArrayList<>();
 
+        try {
+            FileReader fileReader=new FileReader(Regis);
+            BufferedReader bufferedReader=new BufferedReader(fileReader);
+            String ite;
+            while ((ite=bufferedReader.readLine())!=null){
+                String[] reciArray = ite.split(",");
+                String iduser = reciArray[0];
+                String mes = reciArray[1];
+                String item = reciArray[2];
+                String Cantidad = reciArray[3];
+                String valor = reciArray[4];
+                int cant = Integer.parseInt(Cantidad);
+                int val = Integer.parseInt(valor);
+                if (idus.equals(iduser)&&items.equals(item)){
+                    Registroreciclaje ReresObj= new Registroreciclaje(iduser,mes,item,cant,val);
+                    list.add(ReresObj);
+                }
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+    public ArrayList<Registroreciclaje> listconsulplastico (File Regis,String idus,String items){
+        ArrayList<Registroreciclaje> list= new ArrayList<>();
+
+        try {
+            FileReader fileReader=new FileReader(Regis);
+            BufferedReader bufferedReader=new BufferedReader(fileReader);
+            String ite;
+            while ((ite=bufferedReader.readLine())!=null){
+                String[] reciArray = ite.split(",");
+                String iduser = reciArray[0];
+                String mes = reciArray[1];
+                String item = reciArray[2];
+                String Cantidad = reciArray[3];
+                String valor = reciArray[4];
+                int cant = Integer.parseInt(Cantidad);
+                int val = Integer.parseInt(valor);
+                if (idus.equals(iduser)&&items.equals(item)){
+                    Registroreciclaje ReresObj= new Registroreciclaje(iduser,mes,item,cant,val);
+                    list.add(ReresObj);
+                }
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return list;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -115,10 +174,18 @@ public class PantallaEstadisticas extends AppCompatActivity {
         progBarVidrio = findViewById(R.id.progressBarVidrio);
         papeleditext = findViewById(R.id.ttaoeditcart);
         progBarPapel = findViewById(R.id.progressBarPapel);
+        progBarAluminio = findViewById(R.id.progressBarAluminio);
+        ttaoeditalumtex = findViewById(R.id.ttaoeditalum);
+        ttaoeditplasticotext = findViewById(R.id.ttaoeditplastico);
+        progPlastico = findViewById(R.id.progressBarPlastico);
+        materecitext = findViewById(R.id.matemasrecitext);
+
            totalreciclajetextv = findViewById(R.id.Ttaoedittext);
+
 
         Intent receive= getIntent();
         String idf= receive.getStringExtra("idusuario");
+
 
             File fileRead = new File(getFilesDir(),"Reciclaje.txt");
             ArrayList<Registroreciclaje> listregis = listregis(fileRead,idf);
@@ -127,9 +194,14 @@ public class PantallaEstadisticas extends AppCompatActivity {
                     ArrayList<Registroreciclaje> listregis2 = listregis2(fileRead, idf, ite);
                     totalresiclajevidrio(listregis2);
 
-                        ite = "Papeles y Carton";
-                        ArrayList<Registroreciclaje> listregispapel = listregis2(fileRead, idf, ite);
-                        totalresiclajepapel(listregis2);
+                    ite = "Papeles y Carton";
+                    ArrayList<Registroreciclaje> listregispapel = listregis2(fileRead, idf, ite);
+                    ite = "Aluminio";
+                    ArrayList<Registroreciclaje> listconsulaluminio = listregis2(fileRead, idf, ite);
+                    ite = "Plasticos";
+                     ArrayList<Registroreciclaje> listconsulplastico = listregis2(fileRead, idf, ite);
+
+
 
 
 
@@ -138,6 +210,26 @@ public class PantallaEstadisticas extends AppCompatActivity {
         totalresiclaje(listregis);
         totalpesoresiclaje(listregis);
         totalresiclajepapel(listregispapel);
+        totalresiclajeAluminio(listconsulaluminio);
+        totalresiclajePlastico(listconsulplastico);
+
+        int valpap, valplast, valalumn, valvidrio;
+        valpap = progBarPapel.getProgress();
+        valplast = progPlastico.getProgress();
+        valalumn = progBarAluminio.getProgress();
+        valvidrio = progBarVidrio.getProgress();
+
+        if (valpap>valplast&&valpap>valalumn&&valpap>valvidrio){
+            materecitext.setText("Papel y Carton");
+        } else if (valplast>valpap&&valplast>valalumn&&valplast>valvidrio) {
+            materecitext.setText("Plastico");
+        } else if (valalumn>valplast&&valalumn>valpap&&valalumn>valvidrio) {
+            materecitext.setText("Aluminio");
+        } else if (valvidrio>valplast&&valvidrio>valalumn&&valvidrio>valpap) {
+            materecitext.setText("Vidrio");
+        }else {
+            materecitext.setText("Sin determinar");
+        }
 
 
     }
@@ -179,6 +271,32 @@ public class PantallaEstadisticas extends AppCompatActivity {
         progBarPapel.setProgress(totalv);
 
     }
+    public void totalresiclajeAluminio(ArrayList<Registroreciclaje>list){
+        int totalv=0;
+        int max=0;
+        for (Registroreciclaje i: list){
+            totalv+=i.getCantidad();
+            if(max<i.getCantidad()){
+                max=i.getCantidad();
+            }
+        }
+        ttaoeditalumtex.setText(totalv+" KG");
+        progBarAluminio.setProgress(totalv);
+
+    }
+    public void totalresiclajePlastico(ArrayList<Registroreciclaje>list){
+        int totalv=0;
+        int max=0;
+        for (Registroreciclaje i: list){
+            totalv+=i.getCantidad();
+            if(max<i.getCantidad()){
+                max=i.getCantidad();
+            }
+        }
+        ttaoeditplasticotext.setText(totalv+" KG");
+        progPlastico.setProgress(totalv);
+
+    }
     public void totalpesoresiclaje(ArrayList<Registroreciclaje>list){
         int totalp=0;
         int max=0;
@@ -189,9 +307,13 @@ public class PantallaEstadisticas extends AppCompatActivity {
             }
         }
         totextvpesorecic.setText(totalp+" KG");
+        progBarAluminio.setMax(totalp);
+        progPlastico.setMax(totalp);
+        progBarPapel.setMax(totalp);
         progBarVidrio.setMax(totalp);
 
 
     }
+
 
 }
--- app/src/main/java/com/example/proyectoecorecicla/Pantallaconsejos.java
+++ app/src/main/java/com/example/proyectoecorecicla/Pantallaconsejos.java
@@ -1,13 +1,27 @@
 package com.example.proyectoecorecicla;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Pantallaconsejos extends AppCompatActivity {
+    TextView con1, con2, con3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantallaconsejos);
+        con1 = findViewById(R.id.Consejo1text);
+        con2 = findViewById(R.id.Consejo2text);
+        con3 = findViewById(R.id.Consejo3text);
+        consejos();
+    }
+    public void consejos (){
+        con1.setTextSize(22);
+        con2.setTextSize(22);
+        con3.setTextSize(22);
+        con1.setText("Deposita los residuos en el contenedor correspondiente");
+        con2.setText("Limpia los envases de comida antes de tirarlos al contenedor");
+        con3.setText("Utiliza papel resiclado");
     }
 }
